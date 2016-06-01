145c6cf79344eb080b81fd73833d0560481ae600
Marcos Caceres <marcos@marcosc.com>
2016-06-01 17:57:42 +1000

    Feat(tutorial): added template strings spec

---
+++ tutorial/6-template-strings-spec.js
@@ -0,0 +1,27 @@
+"use strict";
+const expect = require('chai').expect;
+const data = [
+  { name: "Capital", link: "/capital" },
+  { name: "Culture", link: "/culture" },
+];
+
+describe("template strings", () => {
+    it("simplifies concatination of strings", function() {
+        function processData(data) {
+            var ul = "<ul>";
+            for (var i = 0; i < data.length; i++) {
+                var item = data[i];
+                var li = '<li><a href="' + item.link;
+                li += '">' + item.name;
+                li += '</a><li>\n';
+                ul += li;
+            }
+            ul += "</ul>";
+            return ul;
+        }
+        const result = processData(data);
+        expect(result).to.equal(
+          `<ul>\n  <li><a href="/capital">Capital</a></li>\n  <li><a href="/culture">Culture</a></li>\n</ul>`
+        );
+    });
+});
