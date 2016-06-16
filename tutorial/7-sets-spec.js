9811104b57ec8ce9ed61a2cc64a48d6c530d5c0b
Tim Godfrey <timg@commoncode.com.au>
2016-06-16 20:24:26 +1000

    Fix up chai assertions

--- tutorial/7-sets-spec.js
+++ tutorial/7-sets-spec.js
@@ -6,15 +6,15 @@ describe("Set", () => {
     //conver to a new Set
     const fruits = ["🍏", "🍊", "🍌", "🍓"];
     const deserts = ["🍰", "🍪", "🍧"];
-    //expect(fruits.has("🍌")).to.be(false); 
-    //expect(fruits.has("🍌")).to.be(true);
-    //expect(fruits.size).to.be(4);
+    //expect(fruits.has("🍌")).to.equal(false); 
+    //expect(fruits.has("🍌")).to.equal(true);
+    //expect(fruits.size).to.equal(4);
     fruits.delete("🍌");
 
     const food = []; // User Array.from() and combine fruits and desets into an array
 
     //empty the set
-    expect(fruits.size).to.be(0);
+    expect(fruits.size).to.equal(0);
   });
 });
 
