0c60a6ba1aa5bcfbd2a8e907a7580b8187bbba80
Marcos Caceres <marcos@marcosc.com>
2016-07-23 18:50:19 +1000

    style: fixup style, remove redudant test

--- tutorial/10-destructuring-spec.js
+++ tutorial/10-destructuring-spec.js
@@ -1,21 +1,13 @@
 "use strict";
-const {expect} = require("chai");
+const { expect } = require("chai");
 
-describe("destructuring", function() {
+describe("destructuring", () => {
   it("allows us to name parts of arrays as variables", () => {
     const person = ["Bob", "W", "Smith"];
     let firstName;
     let lastName;
     expect(firstName).to.equal("Bob");
     expect(lastName).to.equal("Smith");
-  });
-
-  it("allows us to use object's properties as variables", () => {
-    // Get the following values from the global Math object
-    let E;
-    let PI;
-    expect(E).to.equal(Math.E);
-    expect(PI).to.equal(Math.PI);
   });
 
   it("allows us to assign properties to renamed variables", () => {
@@ -32,7 +24,7 @@ describe("destructuring", function() {
 
   it("allows us to assign deep properties into variables", () => {
     const person = {
-      address: {number: "123", street: "Something road"},
+      address: { number: "123", street: "Something road" },
       firstName: "Bob",
       lastName: "Smith",
     };
--- tutorial/11-rest-and-spread-spec.js
+++ tutorial/11-rest-and-spread-spec.js
@@ -1,6 +1,6 @@
-describe("rest", ()=>{
-  it("allows us to group arguments into an array", ()=>{
-    function main(arg1, arg2){
+describe("rest", () => {
+  it("allows us to group arguments into an array", () => {
+    function main(arg1, arg2) {
       expect(arg1).to.equal("a", "b");
       expect(theRest).to.eql(["c", "d"])
     }
@@ -9,8 +9,8 @@ describe("rest", ()=>{
 
 })
 
-describe("spread", ()=>{
-  it("allows us to spread iterables", ()=>{
+describe("spread", () => {
+  it("allows us to spread iterables", () => {
     var set = new Set(["1", "2", "3"]);
     var array1 = [];
     expect(array1).to.eql(["1", "2", "3"]);
